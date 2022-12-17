af7a8cac412536f892c0d6fd47f65f3403ef19f5
EMLuce <emluce@proton.em>
2022-12-17 09:22:38 -0500

    update note has been added.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import json
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, url_for, redirect
 from flask_login import login_required, current_user
 from .models import Note
 
 from app import db
+from .models import Note
 
 views =  Blueprint('views', __name__)
 
@@ -23,7 +24,6 @@ def home():
 
 
     return render_template('home.html', user=current_user, current_date=current_date)
-
 
 @views.route('/delete-note', methods=['POST'])
 @login_required
@@ -54,3 +54,22 @@ def complete_note():
         db.session.commit()
         
     return jsonify({})
+
+@views.route('/update/<int:note_id>', methods=['GET','POST'])
+@login_required
+def update(note_id):
+
+    note = Note.query.filter_by(id=note_id).first()
+
+    if request.form.get('action') == 'Edit': 
+        new_note = request.form.get('note')
+        new_due_date = request.form.get('due-date')
+
+        note.note = new_note
+        note.due_date = new_due_date
+        db.session.commit()
+        return redirect(url_for('views.home'))
+    elif request.form.get('action') == 'Cancel': 
+        return redirect(url_for('views.home'))
+
+    return render_template('update.html', user=current_user, note=note)
